090cb7db2c17c96f65e2cef849f0e57cc76ea2f1
ritvix23 <ee18btech11038@iith.ac.in>
2019-11-08 16:40:54 +0530

    hello_boys_hru?

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("cid").style.top = "-70%";
-    document.getElementById('slider').style.height='15%'
+    document.getElementById('slider').style.height='15%';
+    document.getElementsByclassName('navbar a').style.color='black';
     
-
 
   } else {
     document.getElementById("cid").style.top='0px';
-   document.getElementById('slider').style.height='30%'
+   document.getElementById('slider').style.height='30%';
 
 
   }
 } 
+
+
+
