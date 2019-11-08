4641b5e22930bebaeaa9adf2ed766abf38ef4e37
ritvix23 <ee18btech11038@iith.ac.in>
2019-11-08 13:15:05 +0530

    Help_me

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    document.getElementById("cid").style.top = "-65%";
+    document.getElementById("cid").style.top = "-70%";
     document.getElementById('slider').style.height='15%'
     
 
